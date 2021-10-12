12617e272f66e95b2bce3fc71f4d5f25d2822956
Hugh Graham <hugh.a.graham@gmail.com>
2021-10-12 11:25:48 +0100

    Updates to docs and renamed some stuff... ditching idea of osm helper - not really that helpful

--- R/canvas.R
+++ R/canvas.R
@@ -18,7 +18,7 @@ is.cartesian <- function(x){
   if (!sf::st_is_longlat(x)) {
     return(TRUE)
   } else {
-    warning(paste0("Canvas CRS is not cartesian. This probably doesn't matter",
+    warning(paste0("Canvas projection is not cartesian. This probably doesn't matter",
                    "Be careful of the units you use to specify `res`"))
     return(FALSE)
   }
@@ -31,42 +31,71 @@ set_project_canvas <- function(x){
   options(raytrix.canvas=x)
 }
 
-#' set a global canvas
-#'
-#' ...info
-#'
-#' @export
-set_canvas_globe <- function() {
-    set_project_canvas(list(extent = c(-180, 180,
-                                       -90, 90),
-                            projection = "+proj=longlat +datum=WGS84"))
 
-}
 
 #' set/get the canvas of the rayshader-ratrix project
 #'
 #' To enable the downloading of rayshader-readable data for a desired extent,
 #' it is necessary to set the rayshader-ratrix scene canvas using one of the
-#' `set_canvas_x()` functions.
+#' `set_canvas_x()` functions. See details for additional info.
 #'
-#' To retrieve this information use `get_canvas()`
+#' `set_canvas()` offers the most control over the extent and projection of the
+#' raytrix canvas. Use `set_canvas_world()` for a basic WGS84 extent of the
+#' world; this is deliberately simple, for alternative global projections use
+#' `set_canvas()`. `set_canvas_sf()` sets the canvas from an sf or sfc object;
+#' this can be especially useful when you intend to plot sf features as
+#' overlays. `set_canvas_centroid()` allows for the provision of lat long
+#' coordinates and a buffer (in meters) around that point to set the extent;
+#' this function takes WGS84 coords by default (although this can be changed)
+#' but will always convert to Pseudo Mercator for convenience. To retrieve this
+#' information use `get_canvas()` which returns a list including: `extent`,
+#' `projection` and (if `res` is provided) `dimension`. `canvasExent()` returns
+#' a raytrix extent object which is a copy of a {raster} Extent object. This can
+#' be used in combination with {rayshader} functions such as
+#' `generate_polygon_overlay()` to provide the extent of the topography matrix.
 #'
 #' @name raytrix_set_canvas
-#' @param bounds vector of length 4. e.g. `c(xmin, xmax, ymin, ymax)`
-#' @param crs The Coordinate reference system of the canvas. Eiher a numeric
-#' EPSG code or a proj string such as "+proj=longlat +datum=WGS84".
+#' @param bounds numeric vector of length 4. e.g. `c(xmin, xmax, ymin, ymax)`
+#' @param projection The Coordinate reference system of the canvas. Eiher a
+#' numeric EPSG code or a proj string such as "+proj=longlat +datum=WGS84".
+#'
+#' @examples
+#' # set canvas with Universal Polar Stereographic
+#' set_canvas(c(-4e7,  4e7, -4e7,  4e7 ),'+proj=ups')
+#' get_canvas()
+#' get_canvas(4e4)
+#'
+#' # set canvas with tilted perspective...
+#' set_canvas(c(-3.8e6,  3.8e6, -3.8e6,  3.8e6 ),'+proj=tpers +h=5500000 +lat_0=40')
+#' get_canvas(4e4)
+#'
+#' # set WGS84 global extent
+#' set_canvas_world()
+#' get_canvas(0.7)
+#'
+#' # set canvas for Mt. St Helens
+#' set_canvas_centroid(46.200732, -122.187082, radius = 7000)
+#' get_canvas(10)
+#'
+#' # set canvas with sf object.
+#' library(sf)
+#' demo(nc, ask = FALSE, echo = FALSE)
+#' set_canvas_sf(nc)
+#' get_canvas(7e-3)
+#'
+#' canvas_extent()
 #'
 #' @export
-set_canvas <- function(.bounds, crs){
+set_canvas <- function(bounds, projection){
 
-  is.cartesian(crs)
+  is.cartesian(projection)
 
-  # if (is.numeric(crs)) {       # if EPSG numeric is given convert to wkt
-  #   crs <- sf::st_crs(crs)$wkt
-  # }
+  if (is.numeric(projection)) {       # if EPSG numeric is given convert to wkt
+    projection <- sf::st_crs(projection)$wkt
+  }
 
-  canvas0 <- list(extent = c(.bounds[1], .bounds[2], .bounds[3], .bounds[4]),
-                  projection = crs)
+  canvas0 <- list(extent = c(bounds[1], bounds[2], bounds[3], bounds[4]),
+                  projection = projection)
 
   set_project_canvas(canvas0)
 
@@ -74,7 +103,17 @@ set_canvas <- function(.bounds, crs){
 
 
 #' @name raytrix_set_canvas
-#' @param .sf The sf/sfc object used to set the extent and crs of the canvas
+#'
+#' @export
+set_canvas_world <- function() {
+  set_project_canvas(list(extent = c(-180, 180,
+                                     -90, 90),
+                          projection = "+proj=longlat +datum=WGS84"))
+
+}
+
+#' @name raytrix_set_canvas
+#' @param .sf The sf/sfc object used to set the extent and projection of the canvas
 #' @param mask Default is F. NOT WORKING YET!
 #'
 #' @export
@@ -96,20 +135,20 @@ set_canvas_sf <- function(.sf, mask=F){
 #' @param lat ...
 #' @param long ...
 #' @param radius ...
-#' @param crs ...
+#' @param projection ...
 #'
 #' @export
 set_canvas_centroid <- function(lat, long, radius=5000,
-                                crs="+proj=longlat +datum=WGS84"){
+                                projection="+proj=longlat +datum=WGS84"){
 
   extent_sfc <- sf::st_sfc(sf::st_point(c(long, lat)))%>%
-    sf::st_set_crs(crs)
+    sf::st_set_crs(projection)
 
-  if (!suppressWarnings(is.cartesian(sf::st_crs(crs)))) {
+  if (suppressWarnings(!is.cartesian(sf::st_crs(projection)))) {
     extent_sfc <- extent_sfc %>%
       sf::st_transform(crs=3857)
     warning(paste0('`set_canvas_centroid() `converts coordinates to web mercator for convnience.',
-            'to set a specific canvas extent and crs use `set_canvas()`'))
+            'to set a specific canvas extent and projection use `set_canvas()`'))
   }
 
   extent_sfc <- extent_sfc %>%
@@ -124,6 +163,9 @@ set_canvas_centroid <- function(lat, long, radius=5000,
 
 
 #' @name raytrix_set_canvas
+#'
+#' @param res A resolution (i.e pixel dimensions) in the units of the canvas crs
+#' (`get_canvas()$`)
 #' @export
 get_canvas <- function(res){
 
@@ -149,7 +191,7 @@ get_canvas <- function(res){
 }
 
 
-
+#' @name raytrix_set_canvas
 #' @export
 canvasExent <- function(){
 
--- README.md
+++ README.md
@@ -13,8 +13,8 @@ set_canvas(xmin, ymin, xmax, ymax, crs)  # basic argument for setting canvas
 set_canvas_raster(raster/terra/stars)  # set canvas from a raster data class
 set_canvas_sf(sf/sfc, mask = FALSE)  # set canvas from an sf/sfc object
 set_canvas_centroid(long, lat, radius, crs=4326)  # set canvas from cetroid and radius
-
 get_canvas()  # retrieves the extent and crs parameters if required for additional steps...
+canvasExtent() # get an object of class Extent for using in other {rayshader} functions
 ```
 
 **Data:**
@@ -42,14 +42,9 @@ rt_extent() -get canvas extent as raster::Extent class
 
 *other map overlays:*
 
-Lower priority but would still be cool to be able to overlay maps from the main
-mapping libraries onto a 3D surface easily...
+Lower priority but would still be cool...
 
 ```
-osm_drape() # drape osm data from {osmextract}
-tmap_drape()
-mapsf_drape()
-ggplot_drape()
 plot_3d_mesh()
 ```
 
